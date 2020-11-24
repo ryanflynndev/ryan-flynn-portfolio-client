fd44da513c39cd45bb68d071501c3823df0e61e5
Ryan Flynn <ryanflynn44@gmail.com>
2020-11-24 16:42:51 -0500

    Writing first paragraph

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Navbar from './Navbar'
 import FirstImage from './components/FirstImage'
 import { Typography } from '@material-ui/core';
 
+
 function App() {
   return (
     <div>
       <Navbar/>
       {/* <FirstImage/> */}
-      <Typography>Hey!</Typography>
+      <Typography style={{alignSelf: 'center', marginTop: 20, marginRight: 20, fontSize: 20}}>Hey!</Typography>
     </div>
   );
 }
